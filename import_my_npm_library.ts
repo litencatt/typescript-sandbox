c92e990b8bba196578a118ea85c798ecd56eed12
Nakamura Kosuke <ncl0709@gmail.com>
2019-08-14 22:47:29 +0900

    import is available

--- import_my_npm_library.ts
+++ import_my_npm_library.ts
@@ -2,6 +2,8 @@
 //
 // $ ts-node import_my_npm_library.ts
 // => Hello from TypeScript Library
-const foo = require("my-first-ts-npm-library")
+
+// const foo = require("my-first-ts-npm-library")
+import * as foo from 'my-first-ts-npm-Library'
 
 foo.hello()
