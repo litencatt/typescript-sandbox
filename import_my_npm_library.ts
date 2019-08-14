a7f2bcaebe695b5b3b4a27c939f4cb31413a9461
Nakamura Kosuke <ncl0709@gmail.com>
2019-08-14 11:54:14 +0900

    Add comment

--- import_my_npm_library.ts
+++ import_my_npm_library.ts
@@ -1,3 +1,7 @@
+// require https://github.com/litencatt/my-first-ts-npm-library
+//
+// $ ts-node import_my_npm_library.ts
+// => Hello from TypeScript Library
 const foo = require("my-first-ts-npm-library")
 
 foo.hello()
